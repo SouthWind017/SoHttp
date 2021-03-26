68b8504a3c1b4151f5d94ebf9857392650bc7447
SouthWind017 <65751313+SouthWind017@users.noreply.github.com>
2021-03-26 19:44:44 +0800

    核心头文件

--- SoHttpRequest.h
+++ SoHttpRequest.h
@@ -15,56 +15,50 @@
 
 class SoHttp{
     public:
-        void connect();
+        void start();
         SoHttp(int port);
     private:
         int fd;
+        int port;
         int res;
         int clientfd;
         struct sockaddr_in sever_address;
 };
 
 SoHttp::SoHttp(int port) {
+    this->port = port;
+}
 
+void SoHttp::start() {
     //1.创建TCP
     fd = socket(AF_INET,SOCK_STREAM,0);
 
     if (-1 == fd){
-        printf("SoHttp：Socket创建失败 \n");
         perror("Socket创建失败");
-        return;
     }
-
     //2.设置具体参数
     sever_address.sin_family =AF_INET;//协议族
     sever_address.sin_port = htons(port);//端口
     sever_address.sin_addr.s_addr = INADDR_ANY;//第一种
-    //inet_addr("127.0.0.1") 第二种ip
+    //sever_address.sin_addr.s_addr = inet_addr("127.0.0.1") 第二种ip
     //3.绑定套接字
+    memset(&sever_address,0,sizeof(sever_address));
     res = bind(fd,(struct sockaddr*)&sever_address,sizeof(sever_address));
 
     if (-1 == res){
-        printf("SoHttp：Socket绑定失败 \n");
         perror("Socket绑定失败");
         close(fd);
-        return;
     }
     res = listen(fd,1000);
 
     if (-1 == res){
-        printf("SoHttp：Socket监听失败 \n");
         perror("Socket监听失败");
         close(fd);
-        return;
     }
-
-    printf("SoHttp：服务器启动成功 端口在： %i \n",port);
-
-}
-
-void SoHttp::connect() {
+    printf("SoHttp：服务器启动成功 端口在：%i \n",port);
     while (1){
         //等待连接
+
         clientfd = accept(fd,NULL,NULL);
         //处理连接
         close(clientfd);
