bba6caa3836c5b73269a1a14899d3864407e99ae
SouthWind017 <65751313+SouthWind017@users.noreply.github.com>
2021-03-27 22:03:39 +0800

    核心头文件

--- SoHttpRequest.h
+++ SoHttpRequest.h
@@ -12,13 +12,14 @@
 #include<arpa/inet.h>
 #include<unistd.h>
 #include<string.h>
+#include<assert.h>
 
 class SoHttp{
     public:
         void start();
         SoHttp(int port);
     private:
-        int fd;
+        int sock;
         int port;
         int res;
         int clientfd;
@@ -31,47 +32,25 @@ SoHttp::SoHttp(int port) {
 }
 
 void SoHttp::start() {
-    //1.创建TCP
-    fd = socket(AF_INET,SOCK_STREAM,0);
-
-    if (-1 == fd){
-        perror("Socket创建失败");
-    }
-
-    //2.设置具体参数
-    sever_address.sin_family =AF_INET;//协议族
-    sever_address.sin_port = htons(port);//端口
-//    sever_address.sin_addr.s_addr = INADDR_ANY;//第一种
-    sever_address.sin_addr.s_addr = inet_addr("127.0.0.1"); //第二种ip
-    //3.绑定套接字
-    memset(&sever_address,0,sizeof(sever_address));
-    res = bind(fd,(struct sockaddr*)&sever_address,sizeof(sever_address));
-
-    if (-1 == res){
-        perror("Socket绑定失败");
-        close(fd);
-    }
-    res = listen(fd,20);
-
-    if (-1 == res){
-        perror("Socket监听失败");
-        close(fd);
-    }
-    printf("SoHttp：服务器启动成功 端口在：%i \n",port);
-    struct sockaddr_in clnt_addr;
-    socklen_t clnt_addr_size = sizeof(clnt_addr);
+    bzero(&sever_address, sizeof(sever_address));
+    sever_address.sin_family = PF_INET;
+    sever_address.sin_addr.s_addr = htons(INADDR_ANY);
+    sever_address.sin_port = htons(port);
+    sock = socket(AF_INET,SOCK_STREAM,0);
+    assert(sock>=0);
+    res = bind(sock, (struct sockaddr*)&sever_address,sizeof(sever_address));
+    assert(res != -1);
+    res = listen(sock,1);
+    assert(res != -1);
     while (1){
-        //等待连接
-        printf(" 到这里 \n");
 
-        clientfd = accept(fd, (struct sockaddr*)&clnt_addr, &clnt_addr_size);
-
-
-        //处理连接
+        struct sockaddr_in client;
+        socklen_t client_addrlength = sizeof(client);
+        clientfd = accept(sock, (struct sockaddr*)&client, &client_addrlength);
         handClient(clientfd);
-        //释放资源
-        close(clientfd);
     }
+
+
 }
 void SoHttp::handClient(int fd) {
     char buff[1024*1024] = {0}; //缓存
@@ -98,6 +77,7 @@ void SoHttp::handClient(int fd) {
     int fileFd = open(fileName,O_RDONLY);
     int fileLen = read(fileFd,responselen+response, sizeof(response)-responselen);
     write(fd,response,responselen+fileLen);
+    close(fd);
     close(fileFd);
     sleep(2);
 }
